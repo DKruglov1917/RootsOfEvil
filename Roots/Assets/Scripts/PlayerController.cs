29ccfd3aba273554d62882e5c8ea482f12aba256
Dmytro Kruhlov <72298930+DKruglov1917@users.noreply.github.com>
2023-02-04 21:57:45 +0100

    char cursor follow

--- Roots/Assets/Scripts/PlayerController.cs
+++ Roots/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -8,22 +9,29 @@ public class PlayerController : MonoBehaviour
 
     private CharacterController controller;
     private Vector3 playerVelocity;
-    private float playerSpeed = 2.0f;
+    public float playerSpeed = 2.0f;
     private float gravityValue = -9.81f;
 
     [SerializeField] private int damage;
 
     [SerializeField] private Animator swordAnimator;
 
+    private Vector3 lookDot;
+
+    public GameObject testSphere;
+
     private void Start()
     {
         controller = GetComponent<CharacterController>();
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 
     private void Update()
     {
         MovementControls();
         Attack();
+        Look();
     }
 
     private void MovementControls()
@@ -31,21 +39,26 @@ public class PlayerController : MonoBehaviour
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         controller.Move(move * Time.deltaTime * playerSpeed);
 
-        if (move != Vector3.zero)
-            Look(move);
+        if(!controller.isGrounded)
+            playerVelocity.y += gravityValue * Time.deltaTime;
 
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
-
-        playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
     }
 
-    private void Look(Vector3 dot)
+    private void Look()
     {
-        transform.LookAt(dot);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100, 7))
+        {
+            Debug.Log(hit.transform.name);
+            Debug.Log("hit");
+
+            var direction = (hit.point - transform.position).normalized;
+            direction.y = 0f;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction), 5f);
+
+        }        
     }
 
     private void Attack()
@@ -63,12 +76,12 @@ public class PlayerController : MonoBehaviour
                     hitEnemy.TakeDamage(damage);
                 }
 
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
                 Debug.Log("Did Hit");
             }
             else
             {
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+                //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
                 Debug.Log("Did not Hit");
             }
         }        
